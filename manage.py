04fcf2706dcd4f93d8fdc746d4af022e54e29803
schoi1826 <schoi1826@gmail.com>
2019-10-13 21:57:15 -0400

    Add project folder and edit name to letsgitit

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letsgetit.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letsgitit.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
