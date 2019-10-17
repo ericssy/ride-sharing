55cf03904e0e4a63fe5d0c37fba377c69c63d9d9
Deepak Goel <goel.deepak9@gmail.com>
2019-10-17 15:29:32 -0400

    added a second test

--- tests.py
+++ tests.py
@@ -7,3 +7,6 @@ class RoutesTestCase(unittest.TestCase):
     def test_drivers_routes(self):
         resolver = resolve('/login2/')
         self.assertEqual(resolver.view_name, 'django.views.generic.base.TemplateView')
+    def test_drivers_routes(self):
+        resolver = resolve('/accounts/')
+        self.assertEqual(resolver.view_name, 'django.views.generic.base.TemplateView')
