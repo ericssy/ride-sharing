94f0cf169c89f2a2f01fbea399333ee3c44441da
Deepak Goel <goel.deepak9@gmail.com>
2019-10-17 15:34:49 -0400

    deleted tests

--- tests.py
+++ tests.py
@@ -8,5 +8,5 @@ class RoutesTestCase(unittest.TestCase):
         resolver = resolve('/login2/')
         self.assertEqual(resolver.view_name, 'django.views.generic.base.TemplateView')
     def test_drivers_routes(self):
-        resolver = resolve('/accounts/')
+        resolver = resolve('/')
         self.assertEqual(resolver.view_name, 'django.views.generic.base.TemplateView')
