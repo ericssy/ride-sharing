8857ca9f314088a6cc8b23773a71f654ea47211b
Deepak Goel <goel.deepak9@gmail.com>
2019-10-16 23:39:13 -0400

    temporaily deleted tests

--- tests.py
+++ tests.py
@@ -1,9 +1,3 @@
 from django.test import TestCase
 import unittest
 from django.urls import resolve
-
-
-class RoutesTestCase(unittest.TestCase):
-    def test_drivers_routes(self):
-        resolver = resolve('/drivers/')
-        self.assertEqual(resolver.view_name, 'drivers_landing')
