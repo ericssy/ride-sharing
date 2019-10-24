d03a01c7c28a0327249ce188346f3282d03d9d0e
Siyuan <shensiyuan25@gmail.com>
2019-10-24 10:21:36 -0400

    delete comments

--- Rideshare_app/admin.py
+++ Rideshare_app/admin.py
@@ -8,34 +8,3 @@ from django.contrib.admin.views.main import ChangeList
 admin.site.register(Rider)
 admin.site.register(Driver)
 admin.site.register(Ride)
-
-'''
-class RideChangeList(ChangeList):
-
-    def __init__(self, request, model, list_display,
-        list_display_links, list_filter, date_hierarchy,
-        search_fields, list_select_related, list_per_page,
-        list_max_show_all, list_editable, model_admin, sortable_by):
-
-        super(RideChangeList, self).__init__(request, model,
-            list_display, list_display_links, list_filter,
-            date_hierarchy, search_fields, list_select_related,
-            list_per_page, list_max_show_all, list_editable,
-            model_admin, sortable_by)
-
-        # these need to be defined here, and not in MovieAdmin
-        self.list_display = ['action_checkbox', 'departure_location', 'destination_location', 'date', 'rider']
-        self.list_display_links = ['rider']
-        self.list_editable = ['rider']
-
-
-class RideAdmin(admin.ModelAdmin):
-
-    def get_changelist(self, request, **kwargs):
-        return RideChangeList
-
-    def get_changelist_form(self, request, **kwargs):
-        return RideChangeListForm
-
-admin.site.register(Ride, RideAdmin)
-'''
--- Rideshare_app/forms.py
+++ Rideshare_app/forms.py
@@ -1,9 +1,2 @@
 from django import forms
 from Rideshare_app.models import Rider
-
-'''
-class RideChangeListForm(forms.ModelForm):
-
-    riders = forms.ModelMultipleChoiceField(queryset=Rider.objects.all(),
-        required=False)
-'''
