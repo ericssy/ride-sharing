6047f320df61ed97df2d988e8326585d60c01fea
Siyuan <shensiyuan25@gmail.com>
2019-10-24 11:11:35 -0400

    travis

---
+++ test.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+import unittest
+from django.urls import resolve
+
+
+class RoutesTestCase(unittest.TestCase):
+    def test_drivers_routes(self):
+        resolver = resolve('/login2/')
+        self.assertEqual(resolver.view_name, 'django.views.generic.base.TemplateView')
