2a7ecab8e386e7e8aa19cbd2fea9eec3f8676810
schoi1826 <schoi1826@gmail.com>
2019-11-06 18:42:41 -0500

    New models draft

--- Rideshare_app/models.py
+++ Rideshare_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.forms import ModelForm
+import datetime
 # Create your models here.
 
 class Rider(models.Model):
@@ -31,3 +32,25 @@ class PostRideAsDriverForm(ModelForm):
     class Meta:
         model = Ride
         fields = ['date', "departure_location", "destination_location"]
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField(primary_key = True)
+    venmo = models.CharField(max_length = 30)
+    car_make = models.CharField(max_length = 30)
+    car_model = models.CharField(max_length = 30)
+    # rides = models.ManyToManyField(Trip)
+    # drives = models.ManyToManyField(Trip)
+
+class Trip(models.Model):
+    departure_city = models.CharField(max_length = 30)
+    departure_state = models.CharField(max_length = 30)
+    destination_city = models.CharField(max_length = 30)
+    destination_state = models.CharField(max_length = 30)
+    date = models.DateField()
+    time = models.TimeField()
+    driver = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "driver")
+    seats = models.IntegerField()
+    confirmed_riders = models.ManyToManyField(User, related_name = "confirmed_riders")
+    pending_riders = models.ManyToManyField(User, related_name = "pending_riders")
