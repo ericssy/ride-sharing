14202d27671cf5cfc4130b7717594537371b44ae
DeepakG123 <goel.deepak9@gmail.com>
2019-11-11 16:47:22 -0500

    Finished writing tests

--- tests.py
+++ tests.py
@@ -6,7 +6,39 @@ from Rideshare_app.models import Ride, Rider, Driver, User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect
 
-class RoutesTestCase(unittest.TestCase):
+class RideTestCase1(TestCase):
+    def setUp(self):
+        Driver.objects.create(first_name="Deepak", last_name="Goel", email= "dzg4az@virginia.edu", venmo = "DeepakG", phone_number = "7034988423", car_make = "Chrysler", car_model = "200")
+        Driver1 = Driver.objects.get(email = "dzg4az@virginia.edu")
+        Ride.objects.create(departure_location = "Ashburn, VA", destination_location = "UVA", departure_state = "Virginia", destination_state = "Virginia", date = timezone.now(), time = timezone.now().time(), seats = 4, price = 15, driver = Driver1)
+
+    def test_user(self):
+        Ride1 = Ride.objects.get(departure_location = "Ashburn, VA")
+        RidePrice = Ride1.price
+        RideDriver = Ride1.driver.first_name
+        self.assertEqual(RidePrice,15)
+        self.assertEqual(RideDriver, "Deepak")
+
+
+class UserTestCase1(TestCase):
+    def setUp(self):
+        User.objects.create(first_name="Deepak", last_name="Goel", email= "dzg4az@virginia.edu", venmo = "DeepakG", phone_number = "7034988423", driver = None, rider = None)
+
+    def test_user(self):
+        User1 = User.objects.get(first_name="Deepak")
+        User1_email = User1.email
+        self.assertEqual(User1_email, "dzg4az@virginia.edu")
+
+class UserTestCase2(TestCase):
+    def setUp(self):
+        User.objects.create(first_name="Shubhi", last_name= "Maheshwari", email = "smv9t@virginia.edu", venmo = "ShubhiM", phone_number = "703456789", driver = None, rider = None)
+    def test_user(self):
+        User2 = User.objects.get(email = "smv9t@virginia.edu")
+        User2_venmo = User2.venmo
+        self.assertEqual(User2_venmo, "ShubhiM")
+
+
+class LoginTestCase(unittest.TestCase):
     def test_drivers_routes(self):
         resolver = resolve('/login/')
         self.assertEqual(resolver.view_name, 'django.views.generic.base.TemplateView')
@@ -17,15 +49,26 @@ class RidesListViewTests(TestCase):
         response = self.client.get(reverse('upcoming_rides_list'))
         self.assertQuerysetEqual(response.context['upcoming_rides_list'],[])
 
-class UserTestCases(TestCase):
+
+class IndexTestCase(unittest.TestCase):
+            def test_drivers_routes(self):
+                resolver = resolve('/')
+                self.assertNotEqual(resolver.view_name, 'django.views.generic.base.TemplateView')
+
+class RiderTestCase1(TestCase):
     def setUp(self):
-        User.objects.create(first_name="Deepak", last_name="Goel", email= "dzg4az@virginia.edu", venmo = "DeepakG", phone_number = "7034988423", driver = None, rider = None)
-        User.objects.create(first_name="Shubhi", last_name= "Maheshwari", email = "smv9t@virginia.edu", venmo = "ShubhiM", phone_number = "703456789", driver = None, rider = None)
+        Rider.objects.create(first_name="Deepak", last_name="Goel", email= "dzg4az@virginia.edu", venmo = "DeepakG", phone_number = "7034988423")
 
     def test_user(self):
-        User1 = User.objects.get(first_name="Deepak")
-        User1_email = User1.email
-        self.assertEqual(User1_email, "dzg4az@virginia.edu")
-        User2 = User.objects.get(email = "smv9t@virginia.edu")
-        User2_venmo = User2.venmo
-        self.assertEqual(User2_venmo, "ShubhiM")
+        Rider1 = Rider.objects.get(first_name="Deepak")
+        Rider1_email = Rider1.email
+        self.assertEqual(Rider1_email, "dzg4az@virginia.edu")
+
+class RiderTestCase2(TestCase):
+    def setUp(self):
+        Rider.objects.create(first_name="Shubhi", last_name= "Maheshwari", email = "smv9t@virginia.edu", venmo = "ShubhiM", phone_number = "703456789")
+
+    def test_user(self):
+        Rider2 = Rider.objects.get(email = "smv9t@virginia.edu")
+        Rider2_venmo = Rider2.venmo
+        self.assertEqual(Rider2_venmo, "ShubhiM")
